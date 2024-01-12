8be5d1f55068d3354004e9fb7235abd9055a405a
Vako <vagifhuseynov@gmail.com>
2024-01-12 11:02:21 +0400

    rr

--- application/views/user/includes/footer.php
+++ application/views/user/includes/footer.php
@@ -74,11 +74,11 @@
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="<?php echo base_url('experts_single/7') ?>">sTIMUL</a>, 
+                        &copy; <a class="border-bottom" href="<?php echo base_url('experts_single/11') ?>">sTIMUL</a>, 
 
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
                         <?php echo $this->lang->line('Designed By'); ?> <a class="border-bottom" href="">HTML Codex</a><br><br>
-                        <?php echo $this->lang->line('distributed by'); ?> <a class="border-bottom" href="<?php echo base_url('experts_single/7') ?>">ThemeWagon</a>
+                        <?php echo $this->lang->line('distributed by'); ?> <a class="border-bottom" href="<?php echo base_url('experts_single/11') ?>">ThemeWagon</a>
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <div class="footer-menu">
